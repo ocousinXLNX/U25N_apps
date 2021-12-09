2e06b2191a30d8ab9cfa716d6156f11d29a5c930
olivier <79648103+ocousinXLNX@users.noreply.github.com>
2021-12-09 15:26:34 +0000

    add HLS code
    add HLS code

---
+++ hls/axiSTnonBlockingExample/directives.tcl
@@ -0,0 +1,7 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_top -name axiSTnonBlockingExample "axiSTnonBlockingExample"
+set hls_exec 3 
